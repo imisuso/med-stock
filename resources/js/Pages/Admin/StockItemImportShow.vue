865fe2de93ba1ebca46fa784f5c97135a1fcf920
nongsansanee <nong_com@hotmail.com>
2022-09-02 16:03:46 +0700

    add new menu and design UI show data from excel file

--- resources/js/Pages/Admin/StockItemImportShow.vue
+++ resources/js/Pages/Admin/StockItemImportShow.vue
@@ -14,18 +14,75 @@
         </div>  
  
         <div>
+            <!-- <div class=" w-full flex   bg-blue-900 text-white ">
+                    <div class="  ">
+                       # รหัสพัสดุ
+                    </div >
+                    <div class=" pl-16 ">
+                       <label for=""  class="  text-left">ชื่อพัสดุ </label> 
+                    </div>
+                    <div class=" pl-40">
+                         จำนวนที่สั้งซื้อ 
+                    </div>
+                    <div class=" pl-5 ">
+                         ราคา/หน่วย (บาท)
+                    </div>
+                    <div class=" pl-5 ">
+                        catalog_number-lot_number
+                    </div>
+                                
+                </div> -->
+
             <div class=" border p-1 " 
                 v-for="(item_import,index) in stock_item_import"
                 :key="index"
             >
-                {{item_import.item_code}}-{{item_import.item_name}}
+                <div class=" w-full flex-row md:flex md:justify-between ">
+                    <div class="  ">
+                        {{index+1}}.  {{item_import.item_code}}
+                    </div >
+                    <div class="  ">
+                       <label for=""  class=" text-sm">{{item_import.item_name}} </label> 
+                    </div>
+                    <div class=" ">
+                         จำนวน {{item_import.item_receive}} {{item_import.unit_count}}  
+                    </div>
+                    <div>
+                        ราคา/หน่วย {{item_import.price}} บาท
+                    </div>
+                    <div>
+                        ({{item_import.catalog_number}}-{{item_import.lot_number}})
+                    </div>
+                                
+                </div>
             </div>
             
         </div> 
+        <div class=" mt-2">
+            <button
+            class="  w-full flex justify-center py-2  text-md   font-bold bg-green-300 hover:bg-green-200 focus:outline-none"
+            
+            >
+                   <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
+                    </svg>
+                    เพิ่มพัสดุทั้งหมด
+            </button>
+        </div>
+        <div class=" w-full  mt-2">
+                <label class=" text-red-600">***ถ้าเป็นพัสดุใหม่ รหัสพัสดุต้องไม่ซ้ำกับพัสดุที่มีอยู่แล้ว ถ้ารหัสพัสดุซ้ำระบบจะนำจำนวนสั่งซื้อครั้งนี้ไปบวกเพิ่มให้อัตโนมัติ 
+                    และปรับปรุงข้อมูลราคาให้อัตโนมัติ
+                    แต่ถ้าเป็นรหัสพัสดุใหม่ระบบจะเพิ่มรายการพัสดุเป็นรายการใหม่
+                </label>
+        </div>
+        <div class=" w-full  mt-2">
+                <label for="">
+                    คำถาม ถ้าใบสัญญาสั่งซื้อจะเป็นพัสดุเดิมที่เคยมีในคลังพัสดุอยู่แล้ว เท่านั้นใช่หรือไม่
+                </label>
+        </div>
     </AppLayout>
  </template>
  <script setup>
-//import { ref } from 'vue'
 import AppLayout from '@/Layouts/AppLayout.vue'
 import { useForm } from '@inertiajs/inertia-vue3';
 import { ref } from 'vue';
