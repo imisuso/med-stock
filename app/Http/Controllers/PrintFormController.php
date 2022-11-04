96390c5211cc0a94854e9791800344d29319c35e
nongsansanee <nong_com@hotmail.com>
2022-11-04 16:31:51 +0700

    edit Model name case sensitive

--- app/Http/Controllers/PrintFormController.php
+++ app/Http/Controllers/PrintFormController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\budget;
+use App\Models\Budget;
 use App\Models\ItemTransaction;
 use App\Models\OrderItem;
 use App\Models\OrderPurchase;
--- app/Models/budget.php
+++ app/Models/budget.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
-class budget extends Model
+class Budget extends Model
 {
     use HasFactory;
     protected $table = 'budgets';
