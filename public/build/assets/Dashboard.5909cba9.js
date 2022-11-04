ce158d9dd28550de44bc346f5655a33daa3bda37
nongsansanee <nong_com@hotmail.com>
2022-11-04 20:16:39 +0700

    merge dev to me and build

---
+++ public/build/assets/Dashboard.5909cba9.js
@@ -0,0 +1,1 @@
+import{A as e}from"./AppLayout.dac9b4c1.js";import{o,j as t,e as r,a}from"./app.6fde6ef9.js";import"./_plugin-vue_export-helper.cdc0426e.js";const s=a("div",null,[a("label",{for:""},"HELLO")],-1),i={name:"Dashboard",setup(c){return(l,p)=>(o(),t(e,null,{default:r(()=>[s]),_:1}))}};export{i as default};
