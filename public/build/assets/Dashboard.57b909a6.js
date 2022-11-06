7fbbcc5b4401c037d06dbe94ad3ce1dfdd2882e4
nongsansanee <nong_com@hotmail.com>
2022-11-06 16:51:22 +0700

    merge dev and build

--- public/build/assets/Dashboard.57b909a6.js
+++
@@ -1,1 +0,0 @@
-import{A as e}from"./AppLayout.eebee17f.js";import{o,j as t,e as r,a}from"./app.2c3ec0d4.js";import"./_plugin-vue_export-helper.cdc0426e.js";const s=a("div",null,[a("label",{for:""},"HELLO")],-1),i={name:"Dashboard",setup(c){return(l,p)=>(o(),t(e,null,{default:r(()=>[s]),_:1}))}};export{i as default};
