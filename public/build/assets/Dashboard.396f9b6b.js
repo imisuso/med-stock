a40c9086abf1a8e9cf73cc7dd937f3b1b736db5b
nongsansanee <nong_com@hotmail.com>
2023-02-13 14:14:18 +0700

    merge dev to me

---
+++ public/build/assets/Dashboard.396f9b6b.js
@@ -0,0 +1,1 @@
+import{A as o}from"./AppLayout.0f3df538.js";import{h as t,d as e,o as r,a}from"./app.72e39c28.js";import"./_plugin-vue_export-helper.cdc0426e.js";import"./Navbar.8a28d2f9.js";const s=a("div",null,[a("label",{for:""},"HELLO")],-1),u={name:"Dashboard",setup(p){return(c,l)=>(r(),t(o,null,{default:e(()=>[s]),_:1}))}};export{u as default};
--- public/build/assets/Dashboard.d7685ac4.js
+++
@@ -1,1 +0,0 @@
-import{A as o}from"./AppLayout.fb04cf73.js";import{h as t,d as e,o as r,a}from"./app.9b9bfbb4.js";import"./_plugin-vue_export-helper.cdc0426e.js";import"./Navbar.bf7533c4.js";const s=a("div",null,[a("label",{for:""},"HELLO")],-1),u={name:"Dashboard",setup(p){return(c,l)=>(r(),t(o,null,{default:e(()=>[s]),_:1}))}};export{u as default};
