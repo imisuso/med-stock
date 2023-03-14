a97a23ff57970e19818b58c702c8406941ea3431
nongsansanee <nong_com@hotmail.com>
2023-03-14 16:09:27 +0700

    add font-thai for dompdf

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -8,6 +8,7 @@ use App\Models\StockItem;
 use Illuminate\Http\Request;
 //use PDF;
 use Barryvdh\DomPDF\Facade\Pdf;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class PDFController extends Controller
@@ -21,12 +22,8 @@ class PDFController extends Controller
     public function viewPDF(int $stock_id,int $year,int $month) 
     {
      
-       // $stock_items = StockItem::take(10)->get();
-     //  dd($month);
-    //    $stock_id =12;
-    //    $year=2023;
-    //    $month=2;
-       $stock_item_checkouts = ItemTransaction::where(
+        $thaimonth_short = ['', 'ม.ค.', 'ก.พ.', 'มี.ค.', 'เม.ย.', 'พ.ค.', 'มิ.ย.', 'ก.ค.', 'ส.ค.', 'ก.ย.', 'ต.ค.', 'พ.ย.', 'ธ.ค.'];
+        $stock_item_checkouts = ItemTransaction::where(
                                                 [   'stock_id'=>$stock_id,
                                                     'year'=>$year,
                                                     'month'=>$month,
@@ -40,17 +37,18 @@ class PDFController extends Controller
 
      
 
-     logger($stock_item_checkouts);
-    //  logger($stock_item_checkouts[0]);
-
-    //  $stock_item_checkouts = ItemTransaction::all();
-  //  $tmp_arr =[];
+    // logger($stock_item_checkouts);
+   
         foreach($stock_item_checkouts as $key=>$tran_checkout){
             // logger($tran_checkout);
             //  logger($tran_checkout->id);
             //  logger($tran_checkout->date_action);
             //  logger($tran_checkout['stock_item'][]);
              // logger('------------');
+             $split_date_action = explode('-', $tran_checkout->date_action);
+             $year_print = (int) $split_date_action[0] + 543;
+             $date_action_show = $split_date_action[2].'  '.$thaimonth_short[(int) $split_date_action[1]].' '.$year_print;
+             $stock_item_checkouts[$key]['date_action_show'] = $date_action_show;
                 $date_expire_last = ItemTransaction::query()->select('date_expire')
                                                     ->where(['stock_item_id'=>$tran_checkout->stock_item_id,
                                                                         'action'=>'checkin',
@@ -58,18 +56,17 @@ class PDFController extends Controller
                                                                 ])
                                                     ->orderBy('date_expire','desc')
                                                     ->first();
-               $stock_item_checkouts[$key]['date_expire_last'] = $date_expire_last->date_expire;
+            $split_date_expire = explode('-', $date_expire_last->date_expire);
+            $year_print = (int) $split_date_expire[0] + 543;
+            $date_expire_show = $split_date_expire[2].'  '.$thaimonth_short[(int) $split_date_expire[1]].' '.$year_print;
+
+               $stock_item_checkouts[$key]['date_expire_last'] = $date_expire_show;
 
             // logger($stock_item_checkouts[$key]['date_expire_last']);
           
 
         }
-        //  logger('aaaaaaaaaaaaaaaaaaaaaaaaa');
-        // dd($stock_item_checkouts);
-   
-      
-        // $pdf = Pdf::loadView('pdf.stock_item_view', ['stock_items'=>collect($stock_item_checkouts)->all() ])
-        //                     ->setPaper('a4','landscape');
+ 
         $stock = Stock::find($stock_id);
         $head2 = $stock->stockname."  ภาควิชาอายุรศาสตร์";
        
@@ -77,20 +74,27 @@ class PDFController extends Controller
         $thaimonth = ['', 'มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน', 'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม'];
         $head3 = "เดือน".$thaimonth[(int) $month]."  ปี ".$year+543;
        
-
-        $pdf = Pdf::setOptions([
-                        'isRemoteEnabled' => true, // อนุญาตให้ download font จาก link ได้
-                        // 'fontDir' => storage_path('fonts'), // folder ต้อง exists และมีสิทธิ์ RW
-                        // 'fontCache' => storage_path('fonts'), // folder ต้อง exists และมีสิทธิ์ RW
-                    ])->loadView('pdf.stock_item_view', 
+      
+        // วันเวลาที่พิมพ์
+        $mutable = Carbon::now();
+        //\Log::info($mutable);
+        $tmp_date_now = explode(' ', $mutable);
+        $split_date_now = explode('-', $tmp_date_now[0]);
+        $year_print = (int) $split_date_now[0] + 543;
+        $date_now_show = $split_date_now[2].'  '.$thaimonth[(int) $split_date_now[1]].' '.$year_print;
+        $date_print = 'วันเวลาที่พิมพ์'.'  '.$date_now_show.'  '.$tmp_date_now[1].' น.';
+       
+        $pdf = Pdf::loadView('pdf.stock_item_view', 
                                     [
                                         'stock_items'=>$stock_item_checkouts,
                                         'head2'=>$head2,
                                         'head3'=>$head3,
+                                        'date_print'=>$date_print,
                                     ]
                                 )
                     ->setPaper('a4','landscape');
-        return $pdf->stream('pdf_file.pdf');
+
+        return $pdf->stream('medstock_report_cutstock.pdf');
     }
     public function downloadPDF() 
     {
--- resources/views/pdf/stock_item_view.blade.php
+++ resources/views/pdf/stock_item_view.blade.php
@@ -4,47 +4,107 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   
-    <title>Document</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>ReportCutStock</title>
 
-   
-    
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Sarabun:ital,wght@0,400;0,600;1,400&display=swap" rel="stylesheet">
+  
     <style>
+                @font-face {
+            font-family: 'THSarabunNew';
+            font-style: normal;
+            font-weight: normal;
+            src: url("{{public_path('fonts/sarabun/THSarabunNew.ttf')}}") format('truetype');
+        }
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: normal;
+            font-weight: bold;
+            src: url("{{ public_path('fonts/sarabun/THSarabunNew Bold.ttf') }}") format('truetype');
+        }
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: italic;
+            font-weight: normal;
+            src: url("{{ public_path('fonts/sarabun/THSarabunNew Italic.ttf') }}") format('truetype');
+        }
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: italic;
+            font-weight: bold;
+            src: url("{{ public_path('fonts/sarabun/THSarabunNew BoldItalic.ttf') }}") format('truetype');
+        }
+     
+      
+      
         .font-lobster-two {
             font-family: 'Lobster Two', cursive;
         }
         .font-sarabun {
-            font-family: 'Sarabun', sans-serif;
-            font-size: 14px;
+            font-family: 'THSarabunNew', sans-serif;
+            font-size: 16px;
         }
         h3 {
             text-align: center;
-            font-family: 'Sarabun', sans-serif;
+            font-family: 'THSarabunNew', sans-serif;
             font-size: 18px;
+           
         }
-        p {
+        .p-center {
             text-align: center;
-            font-family: 'Sarabun', sans-serif;
+            font-family: 'THSarabunNew', sans-serif;
+            font-size: 16px;
+            line-height: 0.5;
+        }
+        .p-left {
+            text-align: left;
+            font-family: 'THSarabunNew', sans-serif;
             font-size: 16px;
             line-height: 0.5;
         }
         table {
              width: 100%;
         }
+        .footer {  
+            position: fixed;  
+            left: 10px;  
+            bottom: 5px;  
+            right: 10px;   
+            width: 95%;  
+            /* background-color: gray;  
+            color: white;  
+            text-align: center;   */
+        }  
+        .footer-page {  
+            position: fixed;  
+            right: 20px;  
+            bottom: 2px;    
+           
+         
+        }  
+        .page-number:after {
+            text-align: right;
+            font-family: 'THSarabunNew', sans-serif;
+            font-size: 16px;
+            content: "Page: " counter(page)   ;
+        }
+        .page-break {
+            page-break-after: always;
+          
+        }
+
+
     </style>
 </head>
 
 <body >
+    {{-- <h1>Page 1</h1>
+<div class="page-break"></div>
+<h1>Page 2</h1> --}}
     <div >
         <h3 class="h3" >รายงานบันทึกการตัดสต๊อกวัสดุ</h3>
-        <p class="p " >{{$head2}}</p>
-        <p class="p" >{{$head3}}</p>
-        <!-- ภาษาไทยต้อง style ด้วย font thai ไม่งั้นไม่แสดง -->
-        {{-- <p class="">น้องนีดีใจ ได้ไปคอนเสิร์ตจ้ะ</p>
-        <p class="font-sarabun">น้องนีดีใจที่สุดจิ๊จ๊ะ ได้ไปคอนเสิร์ต</p> --}}
+        <p class="p-center" >{{$head2}}</p>
+        <p class="p-center" >{{$head3}} </p>
+    
         <div >
             <table >
                 <thead>
@@ -65,16 +125,27 @@
                         <th>{{ $key+1}}</th>
                         <td scope="col">{{ $data->stockItem?->item_code }}</td>
                         <td scope="col">{{ $data->stockItem?->item_name }}</td>
-                        <td scope="col">{{ date('d-m-Y', strtotime($data->date_expire)) }}</td>
-                        <td scope="col">{{ Carbon\Carbon::parse($data->date_action)->format('d-m-Y')  }}</td>
+                        <td scope="col">{{ $data->date_expire_last }}</td>
+                        <td scope="col">{{ $data->date_action_show }}</td>
                         <th scope="col">{{ $data->item_count }}</td>
                         <td scope="col">{{ $data->user->name}}</td>
                     </tr>
+                        @if( $key % 10 == 0 && $key !=0 )
+                        <div class="footer-page">
+                            <p class="page-number"><br>   
+                        </div>
+                            <div class="page-break"></div>
+                          
+                        @endif
                     @endforeach
                 </tbody>
             </table>
         </div>
-     
+      
+        <footer class="footer">
+            <p class="p-left">{{$date_print}}<br>   
+        </footer>
+       
     </div>
 </body>
 </html>
